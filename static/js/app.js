b53c9f7c70f9b645c24f722fa886bf690261856b
Scott Mac McEachern <the_fixer@hotmail.com>
2019-05-08 15:22:21 -0700

    Completed metadata

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,18 @@
+/*
+Functionality to build the charts using Plotly.js
+
+Scott McEachern
+May 8, 2019
+*/
+
+
 function buildMetadata(sample) {
+/* Updates metadata information displayed; gets information from endpoint and then updates div
+
+  Accepts : sample (string) unique identifier of the sample to get display the metadata for
+
+  Returns : undefined
+*/
 
   // @TODO: Complete the following function that builds the metadata panel
 
@@ -13,9 +27,39 @@ function buildMetadata(sample) {
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+
+
+    console.log("--> buildMetadata");
+
+
+    d3.json(`/metadata/${sample}`).then((sourceMetadata) => {
+
+      console.log('Received metadata from endpoint');
+
+
+      //- Reference Div to Update
+      var metadataDiv = d3.select("#sample-metadata");
+
+      //- Clear Existing HTML
+      metadataDiv.html("");
+
+
+      //- Insert Metadata
+      Object.entries(sourceMetadata).forEach(([key, value]) => {
+          
+          metadataDiv.append("p").text(`${key}: ${value}`)
+      });
+    });
 }
 
+
 function buildCharts(sample) {
+/*
+
+  Accepts : sample (string) unique identifier of the sample to display chart for
+
+  Returns : undefined
+*/
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
 
@@ -24,9 +68,24 @@ function buildCharts(sample) {
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+
+  console.log("--> buildCharts");
+
+
 }
 
+
 function init() {
+  /* Prepares page; loads the selDataset based on information from Names endpoint and then
+  creates the charts.
+
+    Accepts : nothing
+
+    Returns : undefined
+  */
+
+  console.log("--> init");
+
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
@@ -41,16 +100,32 @@ function init() {
 
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
+
+    console.log(`Create initial plots with first sample: ${firstSample}`);
+
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
 }
 
+
 function optionChanged(newSample) {
+  /* User interactively changes the option dropdown within the UI; update the chants/metadata
+  based on the sample provided.
+
+    Accepts : newSample (string) unique identifier of the sample that was selected
+  */
+
+  console.log("--> optionChanged");
+  console.log(`New sample value: ${newSample}`);
+
+
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
 }
 
-// Initialize the dashboard
+
+
+//- Initialize the dashboard
 init();
